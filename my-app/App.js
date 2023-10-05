68047cef361cd5bb7969b02078d43b4f80d13eb6
JuanPringle <83386505+JuanPringle@users.noreply.github.com>
2023-10-05 13:29:26 -0400

    2nd Iteration
    Created useAuth hook

--- my-app/App.js
+++ my-app/App.js
@@ -6,21 +6,23 @@ import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './Screens/Home'
 import LoginScreen from './Screens/Login'
 import ChatScreen from './Screens/Chat';
+import useAuth from './CustomHooks/useAuth';
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  const user=false;
+  const user = useAuth();
+  console.log(user);
   return (
     <NavigationContainer>
     <Stack.Navigator>
       {user ?
-      <Stack.Screen name='Login' component={LoginScreen}/>
-      :
       <>
         <Stack.Screen name='Home' component={HomeScreen} />
         <Stack.Screen name='Chat' component={ChatScreen}/>
       </>
+      :
+      <Stack.Screen name='Login' component={LoginScreen}/>
       }
     </Stack.Navigator>
     </NavigationContainer>
---
+++ my-app/CustomHooks/useAuth.js
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React, { createContext, useContext } from 'react'
+
+const AuthContext= createContext({});
+export const AuthProvider = ({children}) => {
+  return (
+    <AuthContext.Provider value={{
+      user:"John",
+      }}>
+      {children}
+    </AuthContext.Provider>
+  )
+};
+
+export default function useAuth(){
+  return useContext(AuthContext);
+}
