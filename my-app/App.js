6d504c1f54608b3e7209afeefc125d9b383845e6
JuanPringle <83386505+JuanPringle@users.noreply.github.com>
2023-10-05 12:15:01 -0400

    Fixed 1st Iteration

---
+++ my-app/App.js
@@ -0,0 +1,37 @@
+import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './Screens/Home'
+import LoginScreen from './Screens/Login'
+import ChatScreen from './Screens/Chat';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  const user=false;
+  return (
+    <NavigationContainer>
+    <Stack.Navigator>
+      {user ?
+      <Stack.Screen name='Login' component={LoginScreen}/>
+      :
+      <>
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Chat' component={ChatScreen}/>
+      </>
+      }
+    </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
