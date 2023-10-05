542c5ed875d728288bff2fdca683bcccd7a9a238
JuanPringle <83386505+JuanPringle@users.noreply.github.com>
2023-10-04 22:11:09 -0400

    2nd Iteration
    Test

--- AwesomeProject/StackNavigator.js
+++ AwesomeProject/StackNavigator.js
@@ -1,14 +1,25 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import {Home} from './Screens/Home'
+import {Chat} from './Screens/Chat'
+import {Login} from './Screens/Login'
+
 
 const Stack= createNativeStackNavigator();
 
 const StackNavigator = () => {
+  const user= true;
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        {user?
+          <Stack.Screen name="Login" component={Login}/>:
+        <>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Chat" component={Chat} />
+        </>
+        }
       </Stack.Navigator>
     </NavigationContainer>
   )
