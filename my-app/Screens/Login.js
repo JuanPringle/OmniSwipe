4b28f4b719d2b8831716aa62aa6ee8d73b3caf8d
JuanPringle <83386505+JuanPringle@users.noreply.github.com>
2023-10-06 17:48:51 -0400

    4th Iteration
    Added tw and eas

--- my-app/Screens/Login.js
+++ my-app/Screens/Login.js
@@ -1,12 +1,39 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/core'
+import { ImageBackground } from 'react-native';
 
 const Login = () => {
+  const navigation=useNavigation();
+
+
+  useLayoutEffect(()=>{
+    navigation.setOptions({
+      headerShown: false,
+
+    });
+  },[]);
   return (
     <View>
-      <Text>LoginScreen</Text>
+      <ImageBackground source={require('../assets/OmniSwipelogo.PNG')}
+      style={styles.backgroundImage}>
+
+      </ImageBackground>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1, // Cover the entire screen
+    resizeMode: 'cover', // Stretch or shrink the image as needed
+    justifyContent: 'center', // Center content vertically
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center', // Center content horizontally
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Optional: Add a semi-transparent overlay
+  },
+});
 
 export default Login
