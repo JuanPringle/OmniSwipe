092c82458bef13707b214c06fe4208bf8a895ebb
ChrisCaliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2023-10-06 18:08:27 -0400

    organized

--- my-app/Screens/Login.js
+++ my-app/Screens/Login.js
@@ -4,34 +4,52 @@ import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/core'
 import { ImageBackground } from 'react-native';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCo750NYS_o8V_6JMimyGKOUKs9xVKKw_0",
+  authDomain: "omniswipe-e0411.firebaseapp.com",
+  projectId: "omniswipe-e0411",
+  storageBucket: "omniswipe-e0411.appspot.com",
+  messagingSenderId: "895208776685",
+  appId: "1:895208776685:web:0c18c1990449c21039fde4",
+  measurementId: "G-9N904MQX0S"
+};
+
+
 const Login = () => {
   const navigation=useNavigation();
-
+  
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
   useLayoutEffect(()=>{
     navigation.setOptions({
       headerShown: false,
-
+  
     });
   },[]);
+
+  const styles = StyleSheet.create({
+    backgroundImage: {
+      flex: 1, // Cover the entire screen
+      resizeMode: 'cover', // Stretch or shrink the image as needed
+      justifyContent: 'center', // Center content vertically
+    },
+    container: {
+      flex: 1,
+      alignItems: 'center', // Center content horizontally
+      justifyContent: 'center',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)', // Optional: Add a semi-transparent overlay
+    },
+});
+
+
   return (
     <View>
+      <Text>LoginScreen</Text>
       <ImageBackground source={require('../assets/OmniSwipelogo.PNG')}
       style={styles.backgroundImage}>
 
       </ImageBackground>
-=======
-import React from 'react'
-import { View, Text, TextInput, Button } from 'react-native'
-import {useNavigation} from '@react-navigation/core'
-
-const Login = () => {
-  const navigation=useNavigation();
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  return (
-    <View>
-      <Text>LoginScreen</Text>
       <TextInput
         onChangeText={setUsername}
         value={username}
@@ -46,28 +64,15 @@ const Login = () => {
       <Button
         title="Sign In"
         color="#841584"
+        
       />
       <Button
         title="Sign Up"
         color="#841584"
         onPress={()=>navigation.navigate("SignUp")}
       />
->>>>>>> be5db7354c1012a102cb274aeed86981f1cbc1c5
+
     </View>
-  )
-}
-const styles = StyleSheet.create({
-  backgroundImage: {
-    flex: 1, // Cover the entire screen
-    resizeMode: 'cover', // Stretch or shrink the image as needed
-    justifyContent: 'center', // Center content vertically
-  },
-  container: {
-    flex: 1,
-    alignItems: 'center', // Center content horizontally
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Optional: Add a semi-transparent overlay
-  },
-});
+)};
 
 export default Login
