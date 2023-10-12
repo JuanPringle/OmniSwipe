7a806541f6c7243f97bade66f29da8d4d8659474
ChrisCaliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2023-10-12 12:59:45 -0400

    Added Sign out button

--- my-app/Screens/Home.js
+++ my-app/Screens/Home.js
@@ -1,13 +1,35 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native'
+import { 
+  View, 
+  Text, 
+  Button,
+  TouchableOpacity
+} from 'react-native'
 import {useNavigation} from '@react-navigation/core'
+import { firebase } from '../config'
 
 const Home = () => {
+
+  const logoutUser = async (email, password) => {
+    try {
+      const response = await firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+      })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  
   const navigation=useNavigation();
   return (
     <View>
       <Text>HomeScreen</Text>
       <Button title="Go to Chat" onPress={()=>navigation.navigate("Chat")}/>
+
+      <TouchableOpacity
+        onPress={() => logoutUser()}
+      ><Text style={{fontWeight: 'bold', color: "#FFBF00" , margin: 15, fontSize: 20, }} >Sign Out</Text>
+      </TouchableOpacity>
     </View>
   )
 }
