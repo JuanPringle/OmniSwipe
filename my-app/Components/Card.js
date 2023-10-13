648212a1265fc27ca98f5c853c8746a8fb0e8efc
ChrisCaliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2023-10-13 15:41:13 -0400

    started swipe card

---
+++ my-app/Components/Card.js
@@ -0,0 +1,22 @@
+import React, { useLayoutEffect, useState } from 'react';
+import Swiper from 'react-native-deck-swiper';
+import { 
+    View, 
+    Text, 
+    Image,
+    ImageBackground, 
+    TextInput, 
+    TouchableOpacity, 
+    StyleSheet,
+    TouchableWithoutFeedback,
+    Keyboard
+} from 'react-native';
+
+const Card = () => {
+    return (  
+        <View>
+            <Swiper/>
+        </View>
+    );
+}
+export default Card;
